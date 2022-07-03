888816693398aa75c0257a3166d7f9d98e806f99
rhedgeco <rhedgeco@gmail.com>
2022-07-03 16:13:58 -0700

    Added a SimpleSinGenerator script

---
+++ Assets/Scripts/SimpleSineGenerator.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class SimpleSineGenerator : MonoBehaviour
+{
+    [SerializeField, Range(0, 1)] private float amplitude = 0.5f;
+    [SerializeField, Range(16.35f, 7902.13f)] private float frequency = 261.62f; // middle C
+
+    private int _sampleRate;
+    private long _currentSample;
+
+    private void Awake()
+    {
+        _sampleRate = AudioSettings.GetConfiguration().sampleRate;
+    }
+
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        for (int sample = 0; sample < data.Length; sample += channels)
+        {
+            // get total sample progress
+            long totalSamples = _currentSample + sample / channels;
+
+            // convert sample progress into a phase in the frequency
+            float phase = totalSamples * frequency / _sampleRate % 1;
+
+            // get value of phase on a sine wave
+            float value = Mathf.Sin(phase * 2 * Mathf.PI) * amplitude;
+
+            // populate all channels with the values
+            for (int channel = 0; channel < channels; channel++)
+            {
+                data[sample + channel] = value;
+            }
+        }
+
+        // increase sample progress for next iteration
+        _currentSample += data.Length / channels;
+    }
+}
