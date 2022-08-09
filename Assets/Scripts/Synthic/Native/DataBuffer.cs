9b39c964cdcab42190f224e029fb528a0c6251ed
rhedgeco <rhedgeco@gmail.com>
2022-08-09 12:57:16 -0700

    added general data buffer for external use

---
+++ Assets/Scripts/Synthic/Native/DataBuffer.cs
@@ -0,0 +1,26 @@
+using System.Runtime.InteropServices;
+
+namespace Synthic.Native
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DataBuffer<T> : INativeObject where T : unmanaged
+    {
+        private BufferHandler<T> _buffer;
+
+        public int Length => _buffer.Length;
+        public bool Allocated => _buffer.Allocated;
+
+        public static NativeBox<DataBuffer<T>> Construct(int bufferLength)
+        {
+            DataBuffer<T> buffer = new DataBuffer<T> {_buffer = new BufferHandler<T>(bufferLength)};
+            return new NativeBox<DataBuffer<T>>(buffer);
+        }
+
+        public ref T this[int index] => ref _buffer[index];
+
+        public void CopyTo(T[] managedArray) => _buffer.CopyTo(managedArray);
+        public void CopyTo(ref DataBuffer<T> buffer) => _buffer.CopyTo(buffer._buffer);
+
+        void INativeObject.ReleaseResources() => _buffer.Dispose();
+    }
+}
