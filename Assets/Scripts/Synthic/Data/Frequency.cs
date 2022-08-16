8095c75f1981e3604811d4db9aa9ca4d22abcfe4
rhedgeco <rhedgeco@gmail.com>
2022-08-15 21:18:47 -0700

    Simplified frequency calculation

--- Assets/Scripts/Synthic/Data/Frequency.cs
+++ Assets/Scripts/Synthic/Data/Frequency.cs
@@ -5,10 +5,11 @@ namespace Synthic.Data
     public static class Frequency
     {
         public const float A = 440;
+        private const float ADiv32 = A / 32f;
         
         public static float ConvertFromMidi(int midiNote)
         {
-            return A / 32 * math.pow(2, (midiNote - 9) / 12f);
+            return ADiv32 * math.pow(2, (midiNote - 9) / 12f);
         }
     }
 }
