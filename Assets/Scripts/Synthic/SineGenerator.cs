47122d15c0278ef8b79e0e0a8525468873c4174e
rhedgeco <rhedgeco@gmail.com>
2022-08-19 02:33:07 -0700

    use simpler burstsine compilation

--- Assets/Scripts/Synthic/SineGenerator.cs
+++ Assets/Scripts/Synthic/SineGenerator.cs
@@ -11,39 +11,38 @@ namespace Synthic
     {
         [SerializeField, Range(0, 1)] private float amplitude = 0.5f;
         [SerializeField, Range(16.35f, 7902.13f)] private float frequency = 261.62f; // middle C
-        
+
         private static BurstSineDelegate _burstSine;
-    
+
         private double _phase;
         private int _sampleRate;
-    
+
         private void Awake()
         {
             _sampleRate = AudioSettings.outputSampleRate;
-            if (_burstSine != null) return;
-            _burstSine = BurstCompiler.CompileFunctionPointer<BurstSineDelegate>(BurstSine).Invoke;
+            _burstSine ??= BurstCompiler.CompileFunctionPointer<BurstSineDelegate>(BurstSine).Invoke;
         }
-        
+
         protected override void ProcessBuffer(ref SynthBuffer buffer)
         {
             _phase = _burstSine(ref buffer, _phase, _sampleRate, amplitude, frequency);
         }
-        
+
         private delegate double BurstSineDelegate(ref SynthBuffer buffer,
             double phase, int sampleRate, float amplitude, float frequency);
-        
+
         [BurstCompile]
         private static double BurstSine(ref SynthBuffer buffer,
             double phase, int sampleRate, float amplitude, float frequency)
         {
             // calculate how much the phase should change after each sample
             double phaseIncrement = frequency / sampleRate;
-            
+
             for (int sample = 0; sample < buffer.Length; sample++)
             {
                 // calculate and set buffer sample
                 buffer[sample] = new StereoData((float) (math.sin(phase * 2 * math.PI) * amplitude));
-                
+
                 // increment _phase value for next iteration
                 phase = (phase + phaseIncrement) % 1;
             }
